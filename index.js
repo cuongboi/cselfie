0afac5ac61571e22138970d806f9e099d85e3c1f
cuong nguyen <herculesboi@gmail.com>
2017-04-14 01:59:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'cuong') {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
@@ -28,46 +28,3 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
-
-// Đoạn code xử lý khi có người nhắn tin cho bot
-app.post('/webhook', function(req, res) {
-  var entries = req.body.entry;
-  for (var entry of entries) {
-    var messaging = entry.messaging;
-    for (var message of messaging) {
-      var senderId = message.sender.id;
-      if (message.message) {
-        // Nếu người dùng gửi tin nhắn đến
-        if (message.message.text) {
-          var text = message.message.text;
-          if(text == 'hi' || text == "hello")
-          {
-            sendMessage(senderId, "C-selfie: " + 'Xin Chào');
-          }
-          else{sendMessage(senderId, "C-selfie: " + "Xin lỗi, câu hỏi của bạn chưa có trong hệ thống, chúng tôi sẽ cập nhật sớm nhất.");}
-        }
-      }
-    }
-  }
-
-  res.status(200).send("OK");
-});
-
-// Gửi thông tin tới REST API để Bot tự trả lời
-function sendMessage(senderId, message) {
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {
-      access_token: "EAAUHhKvuq0cBAO9rEq0q6dZCXivaraJCoGEItMrnW63wp27UndLum7ZASIy4f2iLkI7vJ1XV5G0hHZB7rxSAzo4BbjCtDmokiUM7JjDFZBweI6vQb2HXcNNpaYd6li8GiWnmditZCVjBKZA4O8KDGZAZBRGH3CZB4ZAwmbdLjYZBv4qdwZDZD",
-    },
-    method: 'POST',
-    json: {
-      recipient: {
-        id: senderId
-      },
-      message: {
-        text: message
-      },
-    }
-  });
-}
