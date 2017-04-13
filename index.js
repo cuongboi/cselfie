e50ff415e1bfeabbf0521e1e6c92f287f6af3dbe
Cuong Nguyen <heculesvietnam@gmail.com>
2017-04-13 19:06:17 +0700

    rebuild

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ var server = http.createServer(app);
 app.listen(process.env.PORT || 3000);
  
 app.get('/', (req, res) => {
-  res.send("Server chạy ngon lành.");
+  res.send("Hello World!");
 });
  
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'quandeptrai') {
+  if (req.query['hub.verify_token'] === 'cuong') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
@@ -58,7 +58,7 @@ function sendMessage(senderId, message) {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: "EAAUHhKvuq0cBABLYZBPTzYBZCfOgZBg0VnGqbfKyki4evQ5uIPUSZBmN5493zw8KZB60zK2uAfTZCqQUZAbHTm1O40QXVqgWFER5O1c2T1ZCI7I26Fz1OaWxePCUsQQaIMQShbAMJ6ZBa1D0ZCrWOrvhkNP7us4yqRC8AsBUwCklZApcgZDZD",
+      access_token: "EAAUHhKvuq0cBANRK9vGGiZCKa1z8XtUzfUcEtZCgrXFn2qJyl4NzzYZBUUhZB4An1nlZCbvshOn0GFd8mNCT4h8LZCPmo9jggDuaNVfgD98lbNDmYCTelu5j7QKhjUXQAL03fpmxKxNSeCymHRtFCrwcjur2alzSsg7yKA5EX7KAZDZD",
     },
     method: 'POST',
     json: {
