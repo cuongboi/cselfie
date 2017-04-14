d8b4dd5cc2e5c50d80d3aa42c96f4c83d896933b
cuong nguyen <herculesboi@gmail.com>
2017-04-15 00:53:17 +0700

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 app.get('/quest', function (req, res) {
     if(req.query['hoi'].length > 0) {
     sendMessage(1058075870932209, req.query['hoi']);
-    res.send('ok mes send ' + req.query['hoi'])
+    res.send(getrep(req.query['hoi']))
     }
     res.send('No')
 })
@@ -76,19 +76,9 @@ function sendMessage(sender, text) {
         }
     })
 }
-function locdau(t){
-var str = t;
-str= str.toLowerCase();
-str= str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a");
-str= str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e");
-str= str.replace(/ì|í|ị|ỉ|ĩ/g,"i");
-str= str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o");
-str= str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u");
-str= str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y");
-str= str.replace(/"/g,"-inch");
-str= str.replace(/đ/g,"d");
-str= str.replace(/!|@|\$|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\'| |\"|\&|\#|\[|\]|~/g,"-");
-str= str.replace(/-+-/g,"-"); 
-str= str.replace(/^\-+|\-+$/g,"");
-return str;
+function getrep(text) {
+var url = require('http://c-selfie.com/api.php?hoi=' + text);
+var url_parts = url.parse(request.url, true);
+var query = url_parts.query;
+    return query;
 }
