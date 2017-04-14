c0b93decfb2fe77d9f3d0cce4a26446fe26ca867
cuong nguyen <herculesboi@gmail.com>
2017-04-15 02:48:23 +0700

    das

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function sendMessage(sender, text) {
     })
 }
 function getrep() {
-request('http://c-selfie.com/api.php?hoi=daw', function (error, response, body) {
+request('ftp://cselfie:Kimhai1510@c-selfie.com/public_html/api.json', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   console.log('body:', body); // Print the HTML for the Google homepage. 
