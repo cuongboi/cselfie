012eeecd41b479e25d657ca5c9e4231c52001f92
cuong nguyen <herculesboi@gmail.com>
2017-04-14 22:37:54 +0700

    update

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
-// A simple chat bot server
- 
-var logger = require('morgan');
-var http = require('http');
-var bodyParser = require('body-parser');
-var express = require('express');
-var request = require('request');
-var router = express();
- 
-var app = express();
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-var server = http.createServer(app);
- 
-app.listen(process.env.PORT || 3000);
- 
-app.get('/', (req, res) => {
-  res.send("Server chạy ngon lành.");
-});
- 
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'cuong') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-});
- 
-// Đoạn code xử lý khi có người nhắn tin cho bot
-app.post('/webhook', function(req, res) {
-  var entries = req.body.entry;
-  for (var entry of entries) {
-    var messaging = entry.messaging;
-    for (var message of messaging) {
-      var senderId = message.sender.id;
-      if (message.message) {
-        // Nếu người dùng gửi tin nhắn đến
-        if (message.message.text) {
-          var text = message.message.text;
-          if(text == 'hi' || text == "hello")
-          {
-            sendMessage(senderId, "C-selfie: " + 'Xin chào bạn, tôi có thể giúp gì?');
-          }  else{
-           sendMessage(senderId, "C-selfie: " + "Tôi sẽ trả lời bạn sớm nhất.");
-          } 
-        }
-      }
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'cuong') {
+        res.send(req.query['hub.challenge'])
     }
-  }
- 
-  res.status(200).send("OK");
-});
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+         if (text == 'hi' || text == 'hello') {
+         sendMessage(sender, "Hi, Can I Help You")
+         } else {
+         sendMessage(sender, "I will reply soon")
+         }
+            
+        }
+    }
+    res.sendStatus(200)
+})
+
+
  
 // Gửi thông tin tới REST API để Bot tự trả lời
 function sendMessage(senderId, message) {
